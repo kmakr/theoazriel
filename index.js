671c0d8c4e7ed01c33fa35d326a2f29b42caf4f3
kmakr <theonlyeurus@gmail.com>
2025-08-19 11:14:46 +0800

    modify

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function removeSecretClass() {
   const el = document.querySelector("#secret");
   console.log(el);
-  console.log('clicked');
+  console.log("clicked");
   el.classList.remove("secret");
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
